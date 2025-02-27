8e8fee064272d768c97236f7ac01a8765f348343
Matt Holland <matt.holland@plymouth.ac.uk>
2025-02-27 10:52:18 +0000

    Added support for adding and removing layers from map.

--- app.R
+++ app.R
@@ -35,9 +35,9 @@ rm(nc_files)
 
 # assign classes for raster values
 classes <- data.frame(num = sort(unique(unlist(lapply(r_list, values)))),
-                      cat = c("Regular risk", 
-                              "Sporadic risk", 
-                              "Regular and sporadic risk"
+                      cat = c("Risk of regular blooms", 
+                              "Risk of sporadic blooms", 
+                              "Risk of regular and sporadic blooms"
                               )
                       )
 
@@ -45,15 +45,29 @@ classes <- data.frame(num = sort(unique(unlist(lapply(r_list, values)))),
 # UI
 ##################################################################################################################################
 ui <- fluidPage(
-  useShinyjs(),  # Enable shinyjs features
+  useShinyjs(), # Enable shinyjs features
   
-  #plot title
+  # plot title
   titlePanel("UK risk of algal blooms"),
   
-  selectInput('season', 'Select season', choices = c("Please make a selection", tools::toTitleCase(unique(names(r_list)))
-                                                              )
-              ),
-  
+  fluidRow(
+    column(
+      width = 3, # Adjust width as needed
+      selectInput(
+        'season',
+        label = NULL,
+        choices = c("Please select a season", tools::toTitleCase(unique(names(r_list))))
+      )
+    ),
+    column(
+      width = 3, # Adjust width as needed
+      checkboxInput("regular_checkbox", "Regular risk areas", value = TRUE)
+    ),
+    column(
+      width = 3, # Adjust width as needed
+      checkboxInput("sporadic_checkbox", "Sporadic risk areas", value = TRUE)
+    )
+  ),
   leafletOutput("map1")
 )
 
@@ -62,11 +76,12 @@ ui <- fluidPage(
 ##################################################################################################################################
 
 server <- function(input, output, session) {
-  
+
   # React to the selection of Season by selecting the correct raster
   r_season <- reactive({
     if (!is.null(input$season) &&
-      input$season != "Please make a selection") {
+      input$season != "Please select a season") {
+      debug_msg("Filtering raster data to selected season")
       raster_temp <- r_list[[tolower(input$season)]]
       print(paste("Selected season:", input$season))
       return(raster_temp)
@@ -81,20 +96,39 @@ server <- function(input, output, session) {
       debug_msg("Generating map1")
 
       # Create a color palette
-      pal <- colorFactor(palette = brewer.pal(length(unique(r_season()[])), "Set1"), 
+      pal <- colorFactor(palette = c("#38B000", "#CBFE33", "#004B24"),
+                         #palette = c("#FFC100", "#FF7400", "#FF0000"),
+                         #palette = brewer.pal(length(unique(r_season()[])), "Set1"), 
                          domain = r_season()[],
                          na.color = "transparent")
       print(classes)
-      leaflet_temp <- generate_map(r_season(), pal, classes)
+      
+      #filter the raster input to respond to checkboxes
+      if(input$regular_checkbox == TRUE & input$sporadic_checkbox == TRUE){
+        r_temp <- r_season()
+      } else if (input$regular_checkbox == TRUE & input$sporadic_checkbox == FALSE){
+        r_temp <- r_season()
+        r_temp[r_temp == 2] <- NA
+        r_temp[r_temp == 3] <- 1
+      } else if (input$regular_checkbox == FALSE & input$sporadic_checkbox == TRUE){
+        r_temp <- r_season()
+        r_temp[r_temp == 1] <- NA
+        r_temp[r_temp == 3] <- 2
+      } else {
+        r_temp <- NULL
+      }
+      
+      # function for generating the leaflet map
+      leaflet_temp <- generate_map(r_temp, pal, classes)
       
     } else {
+      
       leaflet_temp <- NULL
     }
     return(leaflet_temp)
   })
   
 }
-
 
 #run the app
 shinyApp(ui = ui, server = server)
--- app_functions.R
+++ app_functions.R
@@ -14,9 +14,9 @@ generate_map <- function(x, pal, classes) {
   ))
   
   # generate leaflet
-  temp_map <- leaflet() %>%
-    addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
-    addRasterImage(x, colors = pal, opacity = 0.8) %>%
+  temp_map <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%
+    addRasterImage(x, colors = pal, opacity = 0.6) %>%
     addPolygons(
       lng = unlist(lapply(polygon_coords[[1]], function(coord) coord[1])),
       lat = unlist(lapply(polygon_coords[[1]], function(coord) coord[2])),
@@ -28,7 +28,10 @@ generate_map <- function(x, pal, classes) {
     addLegend(pal = pal, values=values(x),labFormat = labelFormat(
       transform = function(x){classes[which(classes["num"]== x),2]}
       )
-    )
+    ) %>%
+    htmlwidgets::onRender("function(el, x) {
+        L.control.zoom({ position: 'bottomright' }).addTo(this);
+      }")
   
   return(temp_map)
 }
